7f69bd1a916e7028ebd4543c8743761efd2acc5b
Emilio Araos <ea3dev@gmail.com>
2020-02-23 07:06:42 -0700

    table create

--- contacts.py
+++ contacts.py
@@ -1,6 +1,22 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Emilio', 31312434, 'ea3@mail.com')")
+db.execute("INSERT INTO contacts VALUES('Brian', 234234, 'brian@mail.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+for row in cursor:
+    print(row)
+
+cursor.close()
+
+
+db.close()
+
+
+
 
 
 
